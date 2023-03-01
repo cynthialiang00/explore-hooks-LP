098f8d97f97d0ac9989fdcd06f1058b83940d2fd
cynthialiang00 <cynliang00@gmail.com>
2023-03-01 15:29:16 -0800

    pt 2 done

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -9,16 +9,23 @@ function ProductView({ products }) {
   // TODO: Replace with state variable
 
   const [sideOpen, setSideOpen] = useState(true);
+  const [selectedProduct, setSelectedProduct] = useState('');
+  // console.log("setSelectedProduct: ", setSelectedProduct)
+  console.log(selectedProduct);
   return (
     <div className="product-view">
       <div className="product-main-area">
         <h1>Products</h1>
-        <div className="product-list">
+        <div className="product-list" >
           {products.map(item =>
             <ProductListItem
               key={item.id}
               product={item}
-              onClick={() => console.log('SELECT PRODUCT', item)}
+              // update selectedProduct to be item when this item is clicked
+              onClick={() => setSelectedProduct(item)}
+              // if selectedProduct is the same as the item. highlight it
+              isSelected = {item.id === selectedProduct.id}
+              
             />
           )}
         </div>
@@ -31,7 +38,7 @@ function ProductView({ products }) {
             {sideOpen ? '>' : '<'}
           </div>
         </div>
-        <ProductDetails visible={sideOpen} />
+        <ProductDetails visible={sideOpen} product={selectedProduct} />
       </div>
     </div>
   );
